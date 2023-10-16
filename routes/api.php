b7805e265f7e482e93bb795777b3a58bc7a5dfad
bubu-gigi <128198007+bubu-gigi@users.noreply.github.com>
2023-10-16 10:50:36 +0200

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\PlanetController;
+use App\Http\Controllers\StarshipController;
+use App\Http\Controllers\SpecieController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'show');
+    Route::get('/users/{id}', 'get');
+    Route::post('/users', 'store');
+    Route::delete('/users/{id}', 'delete');
+    Route::put('/users/{id}', 'update');
 });
+
+Route::controller(FilmController::class)->group(function () {
+    Route::get('/films', 'show');
+    Route::get('/films/{id}', 'get');
+    Route::post('/films', 'store');
+    Route::delete('/films/{id}', 'delete');
+    Route::put('/films/{id}', 'update');
+});
+
+Route::controller(PlanetController::class)->group(function () {
+    Route::get('/planets', 'show');
+    Route::get('/planets/{id}', 'get');
+    Route::post('/planets', 'store');
+    Route::delete('/planets/{id}', 'delete');
+    Route::put('/planets/{id}', 'update');
+});
+
+Route::controller(SpecieController::class)->group(function () {
+    Route::get('/species', 'show');
+    Route::get('/species/{id}', 'get');
+    Route::post('/species', 'store');
+    Route::delete('/species/{id}', 'delete');
+    Route::put('/species/{id}', 'update');
+});
+
+Route::controller(StarshipController::class)->group(function () {
+    Route::get('/starships', 'show');
+    Route::get('/starships/{id}', 'get');
+    Route::post('/starships', 'store');
+    Route::delete('/starships/{id}', 'delete');
+    Route::put('/starships/{id}', 'update');
+});
+
+Route::controller(VehicleController::class)->group(function () {
+    Route::get('/vehicles', 'show');
+    Route::get('/vehicles/{id}', 'get');
+    Route::post('/vehicles', 'store');
+    Route::delete('/vehicles/{id}', 'delete');
+    Route::put('/vehicles/{id}', 'update');
+});
+
+
+
+
+
+
